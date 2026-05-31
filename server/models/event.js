fb9723e3d5f943c1b9bfc900f7472fdd22a76115
Prince Kumar <dhimanprince262@gmail.com>
2026-05-31 17:56:01 +0530

    added coordinates in event schema

--- server/models/event.js
+++ server/models/event.js
@@ -1,106 +1,107 @@
 import mongoose from "mongoose";
 
-const eventSchema = new mongoose.Schema({
+const eventSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        trim: true,
-        required: true,
+      type: String,
+      trim: true,
+      required: true,
     },
     description: {
-        type: String,
-        trim: true,
-        required: true
+      type: String,
+      trim: true,
+      required: true,
     },
     organization: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Organization',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
     },
     category: {
-        type: String,
-        enum: ['Hackathon', 'Workshop', 'CulturalEvent'],
-        required: true,
+      type: String,
+      enum: ["Hackathon", "Workshop", "CulturalEvent"],
+      required: true,
     },
     thumbnail: {
-        type: String,
+      type: String,
     },
     tags: {
-        type: [String],
-        default: [],
+      type: [String],
+      default: [],
     },
     mode: {
-        type: String,
-        enum: ['online', 'offline'],
-        required: true
+      type: String,
+      enum: ["online", "offline"],
+      required: true,
     },
     venue: {
-        address: String,
-        city: String,
-        state: String,
-        country: String,
+      address: String,
+      city: String,
+      state: String,
+      country: String,
+      coordinates: {
+        lat: Number,
+        lng: Number,
+      },
     },
     onlineLink: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     startDateTime: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
     },
     endDateTime: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
     },
     registrationDeadline: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
     },
     maxParticipants: {
-        type: Number,
-        min: 10,
-        required: true
+      type: Number,
+      min: 10,
+      required: true,
     },
     currentParticipants: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     pricing: {
-        isFree: { type: Boolean, required: true },
-        amount: { type: Number, default: 0 },
-        discount: { type: Number, default: 0 },
-        currency: { type: String, default: "INR", }
+      isFree: { type: Boolean, required: true },
+      amount: { type: Number, default: 0 },
+      discount: { type: Number, default: 0 },
+      currency: { type: String, default: "INR" },
     },
-    sponsors: [
-        { type: String }
-    ],
-    FAQs: [{
+    sponsors: [{ type: String }],
+    FAQs: [
+      {
         question: { type: String, required: true },
-        answer: { type: String, required: true }
-    }],
+        answer: { type: String, required: true },
+      },
+    ],
     views: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     bookmarks: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     specificEvent: {
-        type: mongoose.Schema.Types.ObjectId,
-        refPath: "category"
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "category",
     },
     status: {
       type: String,
-      enum: [
-        "draft",
-        "upcoming",
-        "open",
-        "ongoing",
-        "ended",
-      ],
+      enum: ["draft", "upcoming", "open", "ongoing", "ended"],
       default: "draft",
     },
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
-const Event = mongoose.model('Event', eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 export default Event;
