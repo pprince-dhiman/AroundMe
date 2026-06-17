64fc658c9182460d868378d2e5b79f1eb42e2504
Prince Kumar <dhimanprince262@gmail.com>
2026-06-17 21:45:43 +0530

    created route for new workshop

--- server/routes/workshop.route.js
+++ server/routes/workshop.route.js
@@ -1,11 +1,20 @@
-import express from "express"
+import express from "express";
 import { isLoggedIn } from "../middlewares/userAuth.js";
-import validate from "../middlewares/validate.middleware.js";
+import { validateWorkshop } from "../middlewares/validate.middleware.js";
 import { createWorkshopSchema } from "../validators/workshop.validator.js";
 import { createWorkshopController } from "../controllers/workshop.controller.js";
+import { isOrganizer } from "../middlewares/organizerAuth.js";
+import { uploadEventImage } from "../middlewares/multer.js";
 
 const workshopRouter = express.Router({ mergeParams: true });
 
-workshopRouter.post('/', isLoggedIn, validate(createWorkshopSchema), createWorkshopController);
+workshopRouter.post(
+  "/",
+  isLoggedIn,
+  isOrganizer,
+  uploadEventImage,
+  validateWorkshop(createWorkshopSchema),
+  createWorkshopController,
+);
 
 export default workshopRouter;
