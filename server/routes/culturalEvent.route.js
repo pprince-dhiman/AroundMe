ce8fe9d225e20852ec5da1e32b4310ef9416df52
Prince Kumar <dhimanprince262@gmail.com>
2026-06-17 23:15:20 +0530

    Integrated cloudinary

--- server/routes/culturalEvent.route.js
+++ server/routes/culturalEvent.route.js
@@ -1,11 +1,20 @@
-import express from "express"
+import express from "express";
 import { isLoggedIn } from "../middlewares/userAuth.js";
-import validate from "../middlewares/validate.middleware.js";
+import { validateCulturalEvent } from "../middlewares/validate.middleware.js";
 import { createCulturalEventSchema } from "../validators/culturalEvent.validator.js";
 import { createCulturalEventController } from "../controllers/culturalEvent.controller.js";
+import { isOrganizer } from "../middlewares/organizerAuth.js";
+import { uploadEventImage } from "../middlewares/multer.js";
 
 const culturalEventRouter = express.Router({ mergeParams: true });
 
-culturalEventRouter.post('/', isLoggedIn, validate(createCulturalEventSchema), createCulturalEventController);
+culturalEventRouter.post(
+  "/",
+  isLoggedIn,
+  isOrganizer,
+  uploadEventImage,
+  validateCulturalEvent(createCulturalEventSchema),
+  createCulturalEventController,
+);
 
 export default culturalEventRouter;
