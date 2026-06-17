5b1a811dc525b62548e2a41d258a9970493e465c
Prince Kumar <dhimanprince262@gmail.com>
2026-06-17 21:43:10 +0530

    separated each validation middleware for org, hackathon, workshop and others

--- server/middlewares/validate.middleware.js
+++ server/middlewares/validate.middleware.js
@@ -1,5 +1,18 @@
-// zod schema is passed.
 const validate = (schema) => {
+  // returns a middleware
+  return (req, res, next) => {
+    try {
+      // validate the incoming body
+      req.body = schema.parse(req.body);
+      next();
+    } catch (err) {
+      return res.status(400).json({ success: false, message: err });
+    }
+  };
+}; 
+
+// zod schema is passed.
+export const validateHackathon = (schema) => {
   // returns a middleware
   return (req, res, next) => {
     try {
@@ -23,6 +36,30 @@ const validate = (schema) => {
       next();
     } catch (err) {
       console.log(err.issues);
+      return res.status(400).json({ success: false, message: err });
+    }
+  };
+};
+
+export const validateWorkshop = (schema) => {
+  // returns a middleware
+  return (req, res, next) => {
+    try {
+      // parse the complex data of incoming body (str -> obj/arr)
+      req.body.venue = JSON.parse(req.body.venue);
+      req.body.pricing = JSON.parse(req.body.pricing);
+      req.body.sponsors = JSON.parse(req.body.sponsors);
+      req.body.FAQs = JSON.parse(req.body.FAQs);
+      req.body.instructor = JSON.parse(req.body.instructor);
+      req.body.skillLevel = JSON.parse(req.body.skillLevel);
+      req.body.prerequisites = JSON.parse(req.body.prerequisites);
+      req.body.agenda = JSON.parse(req.body.agenda);
+       
+      // validate the incoming body
+      req.body = schema.parse(req.body);
+      next();
+    } catch (err) {
+      console.log("Issues in validation: ",err);
       return res.status(400).json({ success: false, message: err });
     }
   };
