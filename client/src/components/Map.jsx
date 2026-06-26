6229b3231cfee508f9608b88688f4628e47437d9
Prince Kumar <dhimanprince262@gmail.com>
2026-06-26 23:14:35 +0530

    Common Map container to display map

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function Map({ lat, lon }) {
+  console.log("lat", lat, "lon", lon);
+  return !lat || !lon ? (
+    <></>
+  ) : (
+    <MapContainer
+      center={[lat, lon]}
+      zoom={13}
+      dragging={true}
+      scrollWheelZoom={true}
+      doubleClickZoom={true}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <Marker position={[lat, lon]}>
+        <Popup>Location</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
